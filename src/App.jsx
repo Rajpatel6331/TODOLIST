652a883b3e4aaa4ffc65f9b7977d857e0e55356e
Raj <6331rajvirani@gmail.com>
2024-07-01 13:52:41 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import LocalStorage from './LocalStorage'
+// import Input from './Input'
+// import Display from './Display'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* <Input/>
+     <Display/> */}
+     <LocalStorage/>
+    </>
+  )
+}
+
+export default App
